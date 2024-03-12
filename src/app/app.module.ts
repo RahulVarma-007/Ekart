eb5bdbd9a6cabab1462344211d99172b4faa143b
RahulVarma-007 <dvrv369@gmail.com>
2024-03-12 20:23:36 +0530

    Day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TestComponent } from './test/test.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TopHeaderComponent,
-    TestComponent
+    TestComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // using header as attribute, class, id
+  // selector: '[top-header]',
+  // selector: '.top-header',
   selector: 'top-header',
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.css'
